3a0510edb4d9ae17d2642db5dfa3261e140693b6
JJ <jj.vega86@gmail.com>
2022-11-28 07:19:07 -0500

    👌 IMPROVE: Add transition and disable scroll on open

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,30 +1,49 @@
 import { useState } from "react";
 import hamburger from "../assets/images/icon-hamburger.svg";
 import close from "../assets/images/icon-close-menu.svg";
+import { Transition } from "@headlessui/react";
 
 const MobileNav = () => {
   const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    if (open) {
+      setOpen(false);
+      document.body.style.overflow = "unset";
+    } else {
+      setOpen(true);
+      document.body.style.overflow = "hidden";
+    }
+  };
 
   return (
     <div className="relative">
       {!open && (
         <img
-          onClick={() => setOpen(!open)}
+          onClick={handleToggle}
           className="h-6 w-6 sm:hidden"
           src={hamburger}
           alt="hamburger"
         />
       )}
-      {open && (
-        <div className="min-h-screen min-w-full bg-gray-dark bg-opacity-25 fixed right-0 top-0 transition duration-200">
+      <Transition
+        show={open}
+        enter="transition-opacity duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="min-h-screen min-w-full bg-gray-dark bg-opacity-25 fixed right-0 top-0">
           <img
-            onClick={() => setOpen(!open)}
-            className="h-6 w-6 sm:hidden top-4 right-4 absolute"
+            onClick={handleToggle}
+            className="h-6 w-6 sm:hidden top-8 right-6 absolute"
             src={close}
             alt="close icon"
           />
           <ul
-            onClick={() => setOpen(!open)}
+            onClick={handleToggle}
             className="flex flex-col gap-7 font-bold text-lg bg-white text-black p-5 rounded-lg h-60 w-5/6 absolute top-28 left-10 z-50"
           >
             <a href="#about">
@@ -40,7 +59,7 @@ const MobileNav = () => {
             </a>
           </ul>
         </div>
-      )}
+      </Transition>
     </div>
   );
 };
