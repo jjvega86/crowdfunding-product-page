77594eae3f1c27f8ff854340b7e41f7facaf7364
JJ <jj.vega86@gmail.com>
2022-12-01 16:55:13 -0500

    🐛 FIX: Desktop header sizing issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavLinks from "./NavLinks";
 
 const Header = () => {
   return (
-    <header className="bg-[url(./assets/images/image-hero-mobile.jpg)] sm:bg-[url(./assets/images/image-hero-desktop.jpg)] bg-auto bg-no-repeat h-96 min-w-full">
+    <header className="bg-[url(./assets/images/image-hero-mobile.jpg)] sm:bg-[url(./assets/images/image-hero-desktop.jpg)] bg-auto bg-no-repeat h-96 max-w-screen-xl mx-auto">
       <nav className="text-white flex flex-row justify-between sm:justify-around items-center p-6">
         <p className="text-4xl font-bold">crowdfund</p>
         <MobileNav />
